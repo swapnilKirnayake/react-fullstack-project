2f42fe51af9a84c38631a59e9981b23a87483747
swapnil <swapnilkirnayake@gmail.com>
2026-03-20 00:04:38 +0530

    Add environment variables using dotenv and move DB config to .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     try {
 
-        await mongoose.connect("mongodb://127.0.0.1:27017/react_fullstack");
+        await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
     
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 
 const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes.js");
@@ -13,7 +15,8 @@ app.use(express.json());
 
 app.use("/", userRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
 
     console.log("Server running on port 5000");
 });
